b1604dfdddad3b89e0a17b433e92a389ff60c832
Vince <starryblue7@gmail.com>
2021-11-14 22:31:10 -0800

    add morgan logger & change local db name

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require("morgan");
 const mongoose = require('mongoose');
 const path = require('path')
 
@@ -6,12 +7,13 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitness', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
