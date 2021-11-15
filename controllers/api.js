d2ce5207a4b1b00175d92956f2cac425a510a278
Vince <starryblue7@gmail.com>
2021-11-15 01:10:11 -0800

    add API functionality

--- controllers/api.js
+++ controllers/api.js
@@ -1,20 +1,48 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
+// API routes
+
+// Get workouts data
 router.get("/api/workouts", async (req, res) => {
-    res.json('workouts data')
+    try {
+        const workoutData = await Workout.find({});
+        await workoutData.forEach(workout => workout.durationTotaller());
+        res.status(200).json(workoutData);
+    } catch (err) {
+        res.status(400).json(err);
+    } 
 });
 
+// Add exercise
 router.put("/api/workouts/:id", async (req, res) => {
-    res.json('add exercise')
+    try {
+        const workoutData = await Workout.findByIdAndUpdate(req.params.id, {$push: {'exercises': req.body}});
+        res.status(200).json(workoutData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
+// Add workout
 router.post("/api/workouts", async (req, res) => {
-    res.json('add workout')
+    try {
+        const workoutData = await Workout.create(req.body);
+        res.status(200).json(workoutData);
+    } catch (err) {
+        res.status(400).json(err);
+    } 
 });
 
+// Get workouts in range
 router.get("/api/workouts/range", async (req, res) => {
-    res.json('get workouts in range')
+    try {
+        const workoutData = await Workout.find({day: {$gte: new Date().setDate(new Date().getDate()-7)}}).sort({day: 1}).limit(7);
+        await workoutData.forEach(workout => workout.durationTotaller());
+        res.status(200).json(workoutData);
+    } catch (err) {
+        res.status(400).json(err);
+    } 
 });
 
 module.exports = router;
